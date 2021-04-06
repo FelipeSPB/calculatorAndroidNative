cd6c54dfbfc168e9b937ae787e807fc6df5fc9cb
FelipeSPB <felipebiancalana@outlook.com>
2021-04-05 21:36:01 -0300

    Readme e Splash Implementado

---
+++ app/src/main/java/com/example/calculadora/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculadora.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculadora.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/calculadora/view/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.calculadora.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculadora.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },1000)
+
+    }
+}
